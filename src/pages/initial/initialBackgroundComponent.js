a08a78f2a5c2be7fa0e157fc6c6f725e7fce95bb
Austeja Jakimaviciute <austejajak02@gmail.com>
2023-08-05 16:51:06 +0300

    Added background elements and text, brand name after the phone notch

--- src/pages/initial/initialBackgroundComponent.js
+++ src/pages/initial/initialBackgroundComponent.js
@@ -1,16 +1,15 @@
 import * as React from 'react';
-import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import TransparentLines from '../../assets/initial/transparentLines.svg'
-import SolidLines from '../../assets/initial/solidLines.svg';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import {useCallback} from "react";
-
+import GlobalStyles from "../../components/globalStyles";
 const InitialBackgroundComponent = ()=> {
     const [fontsLoaded] = useFonts({
         'Inter-SemiBold': require('../../assets/Inter-SemiBold.ttf'),
         'Inter-Regular': require('../../assets/Inter-Regular.ttf'),
+        'Inter-Medium': require('../../assets/Inter-Medium.ttf'),
     });
 
     const onLayoutRootView = useCallback(async () => {
@@ -24,19 +23,25 @@ const InitialBackgroundComponent = ()=> {
     }
     return (
         <View style={styles.container}>
-            <LinearGradient colors={['rgba(229, 189, 246, 1)', 'rgba(216, 222, 222, 1)']} style={styles.background} >
-                <ImageBackground source={TransparentLines} style={[styles.logo]} >
-                    <ImageBackground source={SolidLines} style={[styles.logo]} >
-                        <Text style={styles.mainParagraph}>
-                            Atrask save naujai
-                            su kiekvienu drabužiu!
-                        </Text>
-                        <Text style={styles.secondaryParagraph}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
-                            sapien eros. Nulla varius iaculis justo, eget efficitur lorem placerat
-                        </Text>
-                    </ImageBackground>
-                </ImageBackground>
+            <LinearGradient colors={['rgba(229, 189, 246, 1)', 'rgba(216, 222, 222, 1)']} style={styles.background}>
+                <Image source={require('../../assets/initial/transparentLines.png')} style={[styles.transparentLines]}/>
+                <Image source={require('../../assets/initial/solidLines.png')} style={[styles.solidLines]} />
+                <SafeAreaView style={GlobalStyles.droidSafeArea}>
+                    <Text style={[styles.brandText]}>
+                    LoremIpsum
+
+                    </Text>
+                </SafeAreaView>
+                <View style={styles.textContainer}>
+                    <Text style={styles.mainParagraph}>
+                        Lorem ipsum dolor sit amet
+                        consectetur!
+                    </Text>
+                    <Text style={styles.secondaryParagraph}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
+                        sapien eros. Nulla varius iaculis justo, eget efficitur lorem
+                    </Text>
+                </View>
             </LinearGradient>
         </View>
     );
@@ -54,19 +59,30 @@ const styles = StyleSheet.create({
         height: '100%',
         position: 'absolute',
     },
-    logo: {
+    textContainer: {
         flex: 1,
+        flexDirection: 'column',
+        bottom: 60,
+    },
+    transparentLines: {
         justifyContent: 'center',
-        alignItems: 'center',
         width: '100%',
         height: '100%',
+        bottom: 250,
+        position: 'absolute',
+    },
+    solidLines: {
+        width: '100%',
+        height: 300,
+        top: 50,
+        right: 0,
         position: 'absolute',
     },
     mainParagraph:{
         color: '#313131',
         textAlign: 'center',
         fontFamily: 'Inter-SemiBold',
-        fontSize: 38,
+        fontSize: 35,
         fontWeight: '600',
         lineHeight: 45,
     },
@@ -74,14 +90,22 @@ const styles = StyleSheet.create({
         color: '#414141',
         paddingTop: 13,
         paddingHorizontal: 20,
-        textAlign: 'center',
+        textAlign: 'justify',
         fontFamily: 'Inter-Regular',
         fontSize: 11,
         fontStyle: 'normal',
         fontWeight: '400',
         lineHeight: 16,
         letterSpacing: 0.275,
-    }
+    },
+    brandText: {
+        color: '#FFFFFF',
+        opacity: 0.5,
+        fontFamily: 'Inter-Medium',
+        fontSize: 12,
+        fontWeight: 500,
+        alignSelf: 'center',
+    },
 });
 
 export default InitialBackgroundComponent;
