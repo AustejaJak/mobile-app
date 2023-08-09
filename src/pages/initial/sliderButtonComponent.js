e6d8f7e09ed96c9b9ad83fddebf33dd2b507f4a0
Austeja Jakimaviciute <austejajak02@gmail.com>
2023-08-09 23:43:30 +0300

    Installed needed dependencies

--- src/pages/initial/sliderButtonComponent.js
+++ src/pages/initial/sliderButtonComponent.js
@@ -1,10 +1,27 @@
 import * as React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import {useCallback} from "react";
+import { useCallback } from "react";
 import buttonArrow from '../../assets/initial/buttonArrow.png';
-const sliderButtonComponent = ()=> {
+import { GestureHandlerRootView  } from "react-native-gesture-handler";
+import {useAnimatedGestureHandler, useSharedValue} from "react-native-reanimated";
+
+const BUTTON_WIDTH = 198;
+const BUTTON_HEIGHT = 50;
+const BUTTON_PADDING = 10;
+const SWIPEABLE_DIMENSIONS = BUTTON_HEIGHT - 2 * BUTTON_PADDING;
+
+const H_WAVE_RANGE = SWIPEABLE_DIMENSIONS + 2 * BUTTON_PADDING;
+const H_SWIPE_RANGE = BUTTON_WIDTH - 2 * BUTTON_PADDING - SWIPEABLE_DIMENSIONS;
+
+const sliderButtonComponent = ({onToggle})=> {
+    const X = useSharedValue(0);
+    const animatedGestureHandler = useAnimatedGestureHandler({
+        onActive:(e) =>{
+            X.value = e.translationX;
+        }
+    })
     const [fontsLoaded] = useFonts({
         'Inter-Medium': require('../../assets/Inter-Medium.ttf'),
     });
@@ -21,11 +38,13 @@ const sliderButtonComponent = ()=> {
     return (
         <View style={styles.container}>
             <View style={styles.submitButtonPath}>
+                <GestureHandlerRootView  onGestureEvent={animatedGestureHandler}>
                 <View style={styles.circleButtonContainer}>
                     <View style={styles.circleButtonPath}>
                         <Image source={buttonArrow} style={styles.arrowStyle}/>
                     </View>
                 </View>
+                </GestureHandlerRootView >
                 <View style={styles.buttonTextContainer}>
                     <Text style={styles.buttonText}>
                         Pradėkime!
@@ -48,8 +67,8 @@ const styles = StyleSheet.create({
     },
     submitButtonPath: {
         flexDirection: 'row',
-        width: '50%',
-        height: '20%',
+        width: BUTTON_WIDTH,
+        height: BUTTON_HEIGHT,
         backgroundColor: '#313131',
         borderRadius: 30,
         shadowColor: "#000000",
@@ -58,16 +77,16 @@ const styles = StyleSheet.create({
             height: 6,
         },
         shadowOpacity:  0.20,
-        shadowRadius: 5.62,
+        shadowRadius: BUTTON_HEIGHT,
         elevation: 8,
     },
     circleButtonPath: {
         alignItems: 'center',
         justifyContent: 'center',
-        width: 35,
-        height: 35,
+        width: SWIPEABLE_DIMENSIONS,
+        height: SWIPEABLE_DIMENSIONS,
         backgroundColor: 'white',
-        borderRadius: 100,
+        borderRadius: SWIPEABLE_DIMENSIONS,
         marginLeft: 10,
     },
     buttonTextContainer: {
@@ -81,8 +100,8 @@ const styles = StyleSheet.create({
         fontSize: 15,
     },
     arrowStyle: {
-        height: 22,
-        width: 22,
+        height: 15,
+        width: 15,
     }
 });
 
